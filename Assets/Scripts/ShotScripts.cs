c49941f4bdf84828c063208482160dfeae7fa483
SergeyGn <legion68rus@yandex.ru>
2019-10-06 20:35:15 +0300

    добавил выстрел

---
+++ Assets/Scripts/ShotScripts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScripts : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 1;
+    [SerializeField]
+    private bool isEnemyShot = false;
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+    void Update()
+    {
+        
+    }
+}

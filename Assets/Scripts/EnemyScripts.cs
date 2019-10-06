278ca0d7319c76c8f29ce2edde7a7f1cf5290ce5
SergeyGn68 <legion30862@gmail.com>
2019-10-06 20:02:55 +0300

    хуелд

---
+++ Assets/Scripts/EnemyScripts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScripts : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 speed = new Vector2(10, 10);
+    [SerializeField]
+    private Vector2 direction = new Vector2(-1, 0);
+    private Vector2 movement;
+    Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        movement = new Vector2(speed.x * direction.x, speed.y*direction.y); 
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = movement;
+    }
+}
---
+++ Assets/Scripts/PLayerScripts.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerScripts : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 speed = new Vector2(50, 50);
+    private Vector2 movement;
+    Rigidbody2D rigidbody;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float InputX = Input.GetAxis("Horizontal");
+        float InputY = Input.GetAxis("Vertical");
+        movement = new Vector2(speed.x * InputX, speed.y * InputY);
+    }
+    void FixedUpdate()
+    {
+        rigidbody.velocity = movement;
+    }
+}

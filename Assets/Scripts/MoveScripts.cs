b714cbe3cb8c3b47cd04d18a22db257d554ea020
SergeyGn <legion68rus@yandex.ru>
2019-10-14 22:42:11 +0300

    Добавил разных врагов

--- Assets/Scripts/MoveScripts.cs
+++ Assets/Scripts/MoveScripts.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class MoveScripts : MonoBehaviour
 {
     [SerializeField]
-    private Vector2 speed = new Vector2(10, 10); //this is speed
+    private Vector2 speed = new Vector2(10, 10);//this is speed
     
     public Vector2 direction = new Vector2(-1, 0);
 
     private Vector2 movement;
+
+    void Start()
+    {
+        int speedUp = Random.Range(1, 5);
+        speed.x += speedUp;
+        if(speedUp>3)
+        {
+            GetComponent<SpriteRenderer>().color=Color.green;
+        }
+
+    }
     void Update()
     {
         movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
